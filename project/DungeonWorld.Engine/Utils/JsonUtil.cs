5f7e196b999d23fd631deda042b9d50d7a2134ab
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-27 23:50:27 +0100

    add JsonUtil

---
+++ project/DungeonWorld.Engine/Utils/JsonUtil.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace DungeonWorld.Engine.Utils
+{
+    public static class JsonUtil
+    {
+        public static string Serialize(object o)
+        {
+            return JsonSerializer.Serialize(o);
+        }
+
+        public static T Deserialize<T>(string json)
+        {
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
