2d44aaef096f401fcc89d66fc43e0a60edac80db
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-28 06:53:20 +0100

    Code refactor

--- project/DungeonWorld.Core/Models/Attributes.cs
+++ project/DungeonWorld.Core/Models/Attributes.cs
@@ -1,63 +1,65 @@
 ﻿namespace DungeonWorld.Core.Models
 {
-    public enum Attribute
-    {
-        Strength = 0,
-        Dexterity,
-        Constitution,
-        Intelligence,
-        Wisdom,
-        Charisma
-    }
-
     public class Attributes
     {
-        int[] attributes;
+        int Strength;
+        int Dexterity;
+        int Constitution;
+        int Intelligence;
+        int Wisdom;
+        int Charisma;
 
         public Attributes(int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
         {
-            attributes = new int[6] { strength, dexterity, constitution, intelligence, wisdom, charisma };
-        }
-
-        int GetAttribute(Attribute attribute)
-        {
-            return attributes[(int)attribute];
+            Strength = strength;
+            Dexterity = dexterity;
+            Constitution = constitution;
+            Intelligence = intelligence;
+            Wisdom = wisdom;
+            Charisma = charisma;
         }
 
         // based on Dungeon World playbook
-        int GetModifier(int attribute)
+        static int GetModifier(int attribute)
         {
             // -inf to 3
             if (attribute <= 3)
+            {
                 return -3;
+            }
 
             // 4 to 5
             if (attribute <= 5)
+            {
                 return -2;
+            }
 
             // 6 to 8
             if (attribute <= 8)
+            {
                 return -1;
+            }
 
             // 9 to 12
             if (attribute <= 12)
+            {
                 return 0;
+            }
 
             // 13 to 15
             if (attribute <= 15)
+            {
                 return 1;
+            }
 
             // 16 to 17
             if (attribute <= 17)
+            {
                 return 2;
+            }
 
             // 18 to +inf
             return 3;
-        }
-
-        public int GetModifier(Attribute attribute)
-        {
-            return GetModifier(GetAttribute(attribute));
         }
     }
 }
