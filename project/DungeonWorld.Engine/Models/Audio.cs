ed97e86ecd6404b43c630d47f6d1c329c3f3a79d
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-28 05:57:54 +0100

    Add audio aabstraction

---
+++ project/DungeonWorld.Engine/Models/Audio.cs
@@ -0,0 +1,25 @@
+﻿using System.Media;
+
+namespace DungeonWorld.Engine.Models
+{
+    public class Audio
+    {
+        SoundPlayer source;
+
+        public Audio(string filepath)
+        {
+            source = new SoundPlayer(filepath);
+            source.Load();
+        }
+
+        public void Play()
+        {
+            source.Play();
+        }
+
+        public void Stop()
+        {
+            source.Stop();
+        }
+    }
+}
